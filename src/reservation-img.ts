65686ab7d5c48c2e633364ed41a8ec740d06cf6e
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-02-18 21:22:12 +0100

    testing optimization 2

--- src/reservation-img.ts
+++ src/reservation-img.ts
@@ -1,7 +1,7 @@
 import reservation1 from "./assets/images/reservation1.webp"
 import reservation2 from "./assets/images/reservation2.webp"
 import reservation3 from "./assets/images/reservation3.webp"
-import reservation4 from "./assets/images/reservation4.webp"
+// import reservation4 from "./assets/images/reservation4.webp"
 
 
 export const slideImages = [
@@ -17,9 +17,9 @@ export const slideImages = [
     url: reservation3,
     caption: 'Window Table VIP'
   },
-  {
-    url: reservation4,
-    caption: 'Bar Table'
-  },
+  // {
+  //   url: reservation4,
+  //   caption: 'Bar Table'
+  // },
 ];
 
