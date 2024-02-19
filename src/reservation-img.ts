05becc55aa16f7235f2698214d5ffab4feae4633
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-02-19 08:27:22 +0100

    added optimized images

--- src/reservation-img.ts
+++ src/reservation-img.ts
@@ -1,14 +1,14 @@
-import reservation1 from "./assets/images/reservation1.webp"
+// import reservation1 from "./assets/images/reservation1.webp"
 import reservation2 from "./assets/images/reservation2.webp"
 import reservation3 from "./assets/images/reservation3.webp"
 // import reservation4 from "./assets/images/reservation4.webp"
 
 
 export const slideImages = [
-  {
-    url: reservation1,
-    caption: 'Table for 4 Regular'
-  },
+  // {
+  //   url: reservation1,
+  //   caption: 'Table for 4 Regular'
+  // },
   {
     url: reservation2,
     caption: 'Family Table'
