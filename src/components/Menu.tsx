c27edd97fd02c5c40daec9a5340cdfd452b73ce4
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-02-19 08:08:02 +0100

    added svg images

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,7 +9,7 @@ const Menu = () => {
             {menuItems.map((item)=>{
                 return <div className="indiItem">
                     <div className="img">
-                        <img src={item.pictureUrl} alt="" className="menu-img" loading="lazy"/>
+                        <img src={item.pictureUrl} alt="" className="menu-img"/>
                        
                     </div>
                     <h5>{item.name}</h5>
