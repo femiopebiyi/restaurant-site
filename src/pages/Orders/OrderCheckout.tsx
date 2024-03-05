f430ee5d1bb466aa390652bde20d9dd301255ea3
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-03-05 21:07:00 +0100

    empty cart display

--- src/pages/Orders/OrderCheckout.tsx
+++ src/pages/Orders/OrderCheckout.tsx
@@ -1,14 +1,12 @@
-import { useContext, useEffect, useState} from "react"
-import { OrderContext } from "../../context/order-context"
+import { useContext, useEffect, useState } from "react";
+import { OrderContext } from "../../context/order-context";
 import { XIcon } from "lucide-react";
-
-
-
+import { Link } from "react-router-dom";
 
 const OrderCheckout = () => {
-  const {order, removeOrder} = useContext(OrderContext)
-  
-     const [total, setTotal] = useState(0);
+  const { order, removeOrder } = useContext(OrderContext);
+
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     let t = 0;
@@ -17,35 +15,41 @@ const OrderCheckout = () => {
     }
     setTotal(t);
   }, [order]);
-    
-
-
 
   return (
-    <div className="order-checkout">
-      <h1 style={{textAlign: "center"}}>Checkout Page</h1>
-      <div className="single-con">
-      {order.map((item, key)=>{
-        return <div className="single-order" key={key}>
-          
-          <p><XIcon color="red" onClick={()=>{
-            removeOrder(item.id)
-          }}/>{`${item.quantity} ${item.name} ${item.topping !== 'none' ? `with ${item.topping} topping` : ' '}`} </p>
-          <h4>${item.finalPrice}</h4>
+    order.length > 0 ? (
+      <div className="order-checkout">
+        <h1 style={{ textAlign: "center" }}>Checkout Page</h1>
+        <div className="single-con">
+          {order.map((item, key) => (
+            <div className="single-order" key={key}>
+              <p>
+                <XIcon
+                  color="red"
+                  onClick={() => {
+                    removeOrder(item.id);
+                  }}
+                />
+                {`${item.quantity} ${item.name} ${item.topping !== 'none' ? `with ${item.topping} topping` : ' '}`}
+              </p>
+              <h4>${item.finalPrice}</h4>
+            </div>
+          ))}
+
+          <div className="total-con" style={{ display: "flex", justifyContent: "space-between", marginTop: "3em" }}>
+            <h5>Total:</h5>
+            <h5>${total.toFixed(2)}</h5>
+          </div>
+
+          <button className="buy-now">Pay Now</button>
         </div>
-      })}
-
-      <div className="total-con" style={{display: "flex", justifyContent: "space-between", marginTop: "3em"}}>
-        <h5>Total:</h5>
-        <h5>${total.toFixed(2)}</h5>
       </div>
-
-      <button className="buy-now">Pay Now</button>
+    ) : (
+      <div className="empty-cart">
+        <h1>Your Cart is Empty back to order <Link to= "..">page</Link></h1>
       </div>
+    )
+  );
+};
 
-      
-    </div>
-  )
-}
-
-export default OrderCheckout
+export default OrderCheckout;
